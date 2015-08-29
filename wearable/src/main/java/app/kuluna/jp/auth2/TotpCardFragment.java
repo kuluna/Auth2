ae9523d35f5ed0f2cce074a5c463dcd0bde911d3
kuluna <mailkuluna@gmail.com>
2015-08-29 18:02:46 +0900

    カードレイアウトの高さ調整

--- wearable/src/main/java/app/kuluna/jp/auth2/TotpCardFragment.java
+++ wearable/src/main/java/app/kuluna/jp/auth2/TotpCardFragment.java
@@ -1,5 +1,6 @@
 package app.kuluna.jp.auth2;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.wearable.view.CardFragment;
 import android.view.LayoutInflater;
@@ -11,6 +12,8 @@ import android.widget.TextView;
  * キーを表示するCardFragment
  */
 public class TotpCardFragment extends CardFragment {
+    private Drawable defaultCardDrawable;
+    private View rootView;
 
     /**
      * インスタンスを生成します
@@ -30,6 +33,9 @@ public class TotpCardFragment extends CardFragment {
 
     @Override
     public View onCreateContentView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootView = container;
+        defaultCardDrawable = container.getBackground();
+
         return inflater.inflate(R.layout.frame_key, container, false);
     }
 
@@ -39,12 +45,21 @@ public class TotpCardFragment extends CardFragment {
 
         Bundle args = getArguments();
         View v = getView();
+        rootView.setBackground(defaultCardDrawable);
         ((TextView) v.findViewById(R.id.card_account)).setText(args.getString("accountid"));
         ((TextView) v.findViewById(R.id.card_authkey)).setText(args.getString("authkey"));
         if (args.getInt("listorder") > 0) {
             v.findViewById(R.id.card_star).setVisibility(View.VISIBLE);
         } else {
-            v.findViewById(R.id.card_star).setVisibility(View.INVISIBLE);
+            v.findViewById(R.id.card_star).setVisibility(View.GONE);
         }
+    }
+
+    public void startAmbientMode() {
+
+    }
+
+    public void endAmbientMode() {
+
     }
 }
